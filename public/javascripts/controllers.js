a34eea4d67f1897ab62896df3bd37d3811b592a5
sstoimirov <spas.stoimirov@gmail.com>
2016-10-01 17:47:20 +0300

    Spas

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -27,31 +27,59 @@ let controllers = {
                     });
             },
             login() {
-                    templates.compile('login')
-                        .then(function (template) {
-                            $('#container').html('');
-                            $('#container').html(template);
+                templates.compile('login')
+                    .then(function (template) {
+                        $('#container').html('');
+                        $('#container').html(template);
 
-                            $('#btn-login').on('click', function () {
-                                var user = {
-                                    username: $('#tb-username').val(),
-                                    password: $('#tb-password').val()
-                                };
-                                dataService.login(user)
-                                    .then(function () {
-                                        console.log('User logged in!')
-                                    })
-                                    .catch(err => {
-                                        console.log(err);
-                                    })
+                        $('#btn-login').on('click', function () {
+                            var user = {
+                                username: $('#tb-username').val(),
+                                password: $('#tb-password').val()
+                            };
+                            dataService.login(user)
+                                .then(function () {
+                                    console.log('User logged in!');
+                                    toastr.success('User Logged in!')
+                                })
+
+                                .catch(err => {
+                                    console.log(err);
+                                })
+                        })
+                    });
+            },
+            register(){
+                templates.compile('register')
+                    .then(function (template) {
+                        $('#container').html('');
+                        $('#container').html(template);
+
+                        $('#btn-register').on('click', function () {
+                            var user = {
+                                username: $('#tb-username').val(),
+                                password: $('#tb-password').val(),
+                                email: $('#tb-email').val(),
+                                phone: $('#tb-phone').val(),
+                                address: $('#tb-address').val()
+                            };
+                            dataService.register(user)
+                                .then(function () {
+                                    console.log('User registered!');
+                                    toastr.success('User registered')
+                                }).catch(err=> {
+                                console.log(err)
                             })
-                        });
+                        })
+
+                    })
+
             },
             logout() {
 
             },
             user() {
-                
+
             },
             search(params) {
 
@@ -71,4 +99,4 @@ let controllers = {
     }
 };
 
- //export {controllers}
+//export {controllers}

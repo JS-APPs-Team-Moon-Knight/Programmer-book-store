f13d7e6a454be35127bcd42efedc8a20515aec47
Dimitar Pechev <dimpechev@gmail.com>
2016-10-01 14:01:31 +0300

    added user class with some input validation
    Some of the validation could be moved (or better copied) to the UI panel
    dealing with the user data so that it notifies the user immedietly that
    a specific field holds incorrect data.

--- public/javascripts/models.js
+++ public/javascripts/models.js
@@ -58,3 +58,124 @@ class Book {
         this._description = value;
     }
 }
+
+class User {
+    constructor(username, password, firstName, lastName, email, address, phoneNumber) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    get username() {
+        return this._username;
+    }
+    set username(value) {
+        value = _formatInputData(value);
+        if (/[^A-Za-z0-9-_.]/g.test(value)) {
+            throw new Error('Username can only contain only latin letters, numbers, dash, underscore or a dot!');
+        }
+
+        this._username = value;
+    }
+
+    get password() {
+        return this._password;
+    }
+    set password(value) {
+        value = _formatInputData(value);
+        this._password = value;
+    }
+
+    get firstName() {
+        return this._firstName;
+    }
+    set firstName(value) {
+        value = _formatInputData(value);
+        _validateName(value);
+        this._firstName = value;
+    }
+
+    get lastName() {
+        return this._lastName;
+    }
+    set lastName(value) {
+        value = _formatInputData(value);
+        _validateName(value);
+        this._lastName = value;
+    }
+
+    get email() {
+        return this._email;
+    }
+    set email(value) {
+        value = _formatInputData(value);
+        let pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+        if (value.match(pattern).length < 1) {
+            throw new Error('Invalid email address!');
+        }
+
+        this._email = value;
+    }
+
+    get address() {
+        return this._address;
+    }
+    set address(value) {
+        value = _formatInputData(value);
+        this._address = value;
+    }
+
+    get phoneNumber() {
+        return this._phoneNumber;
+    }
+    set phoneNumber(value) {
+        value = _formatInputData(value);
+
+        if (/[^0-9]/g.test(value)) {
+            throw new Error('Invalid telephone number!');
+        }
+
+        this._phoneNumber = value;
+    }
+
+    _formatInputData(value) {
+        value = value.trim();
+        var illegalSymbolsMap = {
+            "&": "&amp;",
+            "<": "&lt;",
+            ">": "&gt;",
+            '"': '&quot;',
+            "'": '&#39;',
+            "/": '&#x2F;'
+        };
+
+        function escapeHtml(str) {
+            return String(str).replace(/[&<>"'\/]/g, function (s) {
+                return entityMap[s];
+            });
+        }
+
+        value = escapeHtml(value);
+        return value;
+    }
+
+    _validateName(name) {
+        if (/^[^A-Z]/.test(name)) {
+            throw new Error('Name must begin with a capital letter!');
+        }
+
+        for (let i = 1; i < name.length; i += 1) {
+            if (/^a-z-/g.test(name[i])) {
+                throw new Error('Disallowed characters! A name must begin with a capital letter and continue with lower case latin symbols!');
+            }
+
+            if (name[i - 1] === '-' && name[i] === '-') {
+                throw new Error('There cannot be two or more successive dashes in a name!');
+            }
+        }
+    }
+}
